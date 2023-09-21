a8877173138b39def91894b80bd431b35221e8c7
Ayush Mann <136476855+AyushMann29@users.noreply.github.com>
2023-09-21 21:12:33 +0530

    Add files via upload

---
+++ My page/Script1.js
@@ -0,0 +1,12 @@
+
+function search(){
+    var term = document.getElementById("search").value;
+    if (term === "Ayush Mann"| term === "AYUSH MANN"|term === "Ayush"|term === "ayush mann"|term === "ayush"|term === "AYUSH")
+    {
+        window.location.href = "Second.html";
+    }
+    else
+    {
+        window.location.href="https://www.google.com/"
+    }
+} 
