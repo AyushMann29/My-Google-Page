fea0818224594b9f8a3fc700f62335e52858fb76
Ayush Mann <136476855+AyushMann29@users.noreply.github.com>
2023-12-08 21:54:57 +0530

    Update Script1.js

--- My page/Script1.js
+++ My page/Script1.js
@@ -7,6 +7,31 @@ function search(){
     }
     else
     {
-        window.location.href="https://www.google.com/"
+        window.location.href="https://www.google.com/" + term;
+    }
+}
+
+function search(){
+    const term = document.getElementById("address").value;
+    if (term === "Ayush Mann"| term === "AYUSH MANN"|term === "Ayush"|term === "ayush mann"|term === "ayush"|term === "AYUSH")
+    {
+        window.location.href = "Second.html";
+        document.getElementsByClassName("address2")[0].value = term;
+    }
+    else
+    {
+        window.location.href="https://www.google.com/" + term;
     }
 } 
+
+function Github(){
+    window.location.href="https://github.com/AyushMann29/AyushMann29/";
+}
+
+function carrd(){
+    window.location.href="https://ayurep.carrd.co/";
+}
+
+function Itch(){
+    window.location.href="https://reproducibletie1.itch.io/rock-paper-scissor/";
+}
